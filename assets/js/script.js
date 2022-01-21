8f9f22865c801011148a55eb7fba448a0bd99b08
Elías Ortiz <eortiz3837@e-camp.cl>
2022-01-21 00:13:13 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+	$("a").click(function(){
+		var numeral=this.hash
+	
+		$("html,body").animate({
+			scrollTop: $(numeral).offset().top -20
+		},1000)
+	
+		$(".navbar").css({"background-color":"#0dcaf0"})
+	
+	});
+	
+	$(window).scroll(function(){
+		$(".navbar").css({"background-color":"#0dcaf0"})
+	});
+});
